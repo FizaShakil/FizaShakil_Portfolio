837c113e0efba7763653bc2c63ad1cea17f558f4
FizaShakil <fizashakil25@gmail.com>
2025-08-15 12:48:18 +0500

    Edited routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Home from './components/Home'
 import Projects from './components/Projects'
 import Navbar from './components/Navbar'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import { useLocation } from 'react-router'
+import Services from './components/Services'
 
 function App() {
 
@@ -10,13 +14,27 @@ function App() {
     <>
     <Router>
       <Navbar />
+      <ScrollToTop/>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/projects' element={<Projects />} />
+        <Route path='/services' element={<Services/>}/>
+        <Route path='/projects' element={<Projects/>} />
       </Routes>
+      <Contact/>
+      <Footer/>
     </Router>
     </>
   )
 }
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+      window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 export default App
