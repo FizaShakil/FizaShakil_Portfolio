d000c6691ae5851afcd7f4ebec4a91c62be89e5d
FizaShakil <fizashakil25@gmail.com>
2025-08-17 13:41:53 +0500

    Edited tag in about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,10 +13,10 @@ const About = () => {
           <img src={myImage} alt="about" className='w-full h-full object-cover rounded-xl shadow-2xl' />
         </div>
         <div className='w-[80%] md:w-[45%]'>
-          <h1 className='text-gray-200 mt-7 text-sm md:text-base md:ml-9 leading-relaxed'>
+          <div className='text-gray-200 mt-7 text-sm md:text-base md:ml-9 leading-relaxed'>
          Passionate and dedicated Full stack web developer, helping businesses and entrepreneurs turn their ideas into responsive, secure, and scalable websites that not only look great but also work seamlessly for their users. <br /><br />
          From smooth navigation to strong security measures, I focus on creating solutions that solve real challenges—whether it’s improving user experience, boosting performance, or ensuring your site grows with your business.
-          </h1>
+          </div>
         </div>
       </div>
     </div>
